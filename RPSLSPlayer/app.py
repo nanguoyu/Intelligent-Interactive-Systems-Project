8a1fad4941bca1e8e2c8c5db1fc09d268bf902e0
Dong <5096749+nanguoyu@users.noreply.github.com>
2020-05-05 14:55:38 +0200

    Create the API interface and its test.

--- RPSLSPlayer/app.py
+++ RPSLSPlayer/app.py
@@ -3,11 +3,28 @@
 @Author: Dong Wang
 @Date : 2020/5/5
 """
-from flask import Flask
+from flask import Flask, request
+import base64
+import numpy as np
+import cv2
 from . import __version__
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello, this is the server of RPSLSPlayer agent. \n Version: '+__version__
+    return 'Hello, this is the server of RPSLSPlayer agent. \n Version: ' + __version__
+
+
+@app.route("/gesture", methods=['POST', 'GET'])
+def gesture():
+    image = request.form.get('image')
+    if not image:
+        return 'not found image file'
+    img = base64.b64decode(str(image))
+    image_data = np.fromstring(img, np.uint8)
+
+    # Here we should do something to recognize the image_data
+
+    return {'code': 200, 'gesture': 'open_palm'}
---
+++ tests/test_app.py
@@ -0,0 +1,20 @@
+"""
+@File : test_app.py
+@Author: Dong Wang
+@Date : 2020/5/5
+"""
+import base64
+import json
+import requests
+
+
+def test_post_image(img_file, URL):
+    """ post image and return the response """
+    with open(img_file, 'rb') as f:
+        img = base64.b64encode(f.read()).decode()
+    response = requests.post(URL, data={"image": img})
+    return json.loads(response.content.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    print(test_post_image('openPalm.png', 'http://127.0.0.1:5000/gesture'))
