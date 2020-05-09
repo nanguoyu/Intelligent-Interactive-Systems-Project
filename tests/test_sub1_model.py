e37f6d1302e7482806553f61ea3921be745b4184
nanguoyu <admin@nanguoyu.com>
2020-05-09 17:05:59 +0200

    Test for Subsystem 1

---
+++ tests/test_sub1_model.py
@@ -0,0 +1,20 @@
+"""
+@File : test_sub1_model.py
+@Author: Dong Wang
+@Date : 2020/5/9
+"""
+from RPSLSPlayer.core.extraction import landmarkExtraction, load_img, img_to_array
+import pytest
+
+
+def test_sub1_model():
+    # Model Path
+    imageFile = 'fist_dorsal.jpg'
+    lE = landmarkExtraction(model='./Sub1-weights-best.hdf5')
+    img = load_img(imageFile)
+    re = lE.extract(img)
+    print(re)
+
+
+if __name__ == '__main__':
+    test_sub1_model()
