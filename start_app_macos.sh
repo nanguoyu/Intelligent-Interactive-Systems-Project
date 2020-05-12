9a7bd98db09eb937019015d8ef4e068c4de6c9b6
Dong <5096749+nanguoyu@users.noreply.github.com>
2020-05-12 15:16:42 +0200

    Update start_app_macos.sh.

--- start_app_macos.sh
+++ start_app_macos.sh
@@ -1,1 +1,11 @@
-#!/bin/bashLANG=en_US.UTF-8if [ "$(uname)" == "Darwin" ]then    echo "[App]: Start server in MacOS"elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]then    echo "[App]: Start server in GNU/Linux"fiexport FLASK_APP=./RPSLSPlayer/app.pyflask run
+#!/bin/bash
+LANG=en_US.UTF-8
+if [ "$(uname)" == "Darwin" ]
+then    
+	echo "[App]: Start server in MacOS"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
+then    
+	echo "[App]: Start server in GNU/Linux"
+fi
+export FLASK_APP=./RPSLSPlayer/app.py
+flask run
